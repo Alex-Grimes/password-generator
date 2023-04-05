1b05425063dd80e508d89fdb89c0330485043226
alex <alexgrimes.se@gmail.com>
2023-04-05 17:52:10 -0400

    created generate password function

--- password-generator/createPassword.go
+++ password-generator/createPassword.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"math/rand"
+	"time"
+)
+
+// randomizethe seed for rand so that our deterministic generator dosent produce the same sequences everytime
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
 const voc string = "abcdfghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 const numbers string = "0123456789"
 const symbols string = "!@#$%&*+_-="
@@ -20,5 +30,10 @@ func createPassword(length int, hasNumbers bool, hasSymbols bool) string {
 //Todo create generate Password function
 
 func generatePassword(length int, chars string) string {
-	return "awesome"
+	password := ""
+	for i := 0; i < length; i++ {
+		password += string([]rune(chars)[rand.Intn(len(chars))])
+	}
+
+	return password
 }
