12eed8df1a8bb4c958195665cc839826c6f04b70
alex <alexgrimes.se@gmail.com>
2023-04-07 13:40:27 -0400

    working towards making this an api instead of exe

--- password-generator/main.go
+++ password-generator/main.go
@@ -10,31 +10,30 @@ import (
 )
 
 func main() {
-	log.SetPrefix("Error:")
-	log.SetFlags(0)
-
-	fmt.Println("Creating Password")
-
-	var length = flag.Int("length", 8, "Length of the password")
-	var symbols = flag.Bool("symbols", true, "Include symbols in the password")
-	var numbers = flag.Bool("numbers", true, "Include numbers in the password")
-
-	flag.Parse()
-
-	fmt.Printf("Length: %v, Numbers: %v, Symbols: %v \n", *length, *numbers, *symbols)
-
-	password := createPassword(*length, *numbers, *symbols)
-	fmt.Println("Password Created: ", password)
-
-	savePassword(password)
-	error := clipboard.WriteAll(password)
-	if error != nil {
-		log.Fatal(error)
-	} else {
-		fmt.Println("Password copied to clipboard")
-	}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.SetPrefix("Error:")
+		log.SetFlags(0)
 
+		fmt.Println("Creating Password")
+
+		var length = flag.Int("length", 8, "Length of the password")
+		var symbols = flag.Bool("symbols", true, "Include symbols in the password")
+		var numbers = flag.Bool("numbers", true, "Include numbers in the password")
+
+		flag.Parse()
+
+		fmt.Printf("Length: %v, Numbers: %v, Symbols: %v \n", *length, *numbers, *symbols)
+
+		password := createPassword(*length, *numbers, *symbols)
+		fmt.Println("Password Created: ", password)
+
+		savePassword(password)
+		error := clipboard.WriteAll(password)
+		if error != nil {
+			log.Fatal(error)
+		} else {
+			fmt.Println("Password copied to clipboard")
+		}
 	})
 }
