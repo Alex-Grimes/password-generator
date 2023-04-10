1c7e9697e7b85dbb70f7ffb289fbf5129254e854
alex <alexgrimes.se@gmail.com>
2023-04-09 20:19:44 -0400

    basic html outline to start

--- password-manager/src/app/page.tsx
+++ password-manager/src/app/page.tsx
@@ -1,124 +1,153 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Image from 'next/image';
+import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <main>
+      <header>
+        <nav>
+          <div className='brand'>
+            <a href=''>
+              <i className='fa fa-lock' aria-hidden='true'></i>&nbsp;Password
+              Manager
+            </a>
+          </div>
+          <ul>
+            <li>
+              <a href=''>Home</a>
+            </li>
+            <li>
+              <a></a>
+            </li>
+            <li>
+              <a>SignUp</a>
+            </li>
+            <li>
+              <a>Logout</a>
+            </li>
+            <form action='.' method='POST'>
+              <input type='submit' id='logout-btn' name='logout' />
+            </form>
+
+            <li>
+              <a href=''>View</a>
+            </li>
+            <li>
+              <a href=''>Add</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+
+      {/* <!--Display messages-->
+    <div class="messages" style="text-align: center;">
+          <p>
+
+                <i class="fa fa-exclamation" aria-hidden="true"></i>
+
+                <i class="fa fa-check" aria-hidden="true"></i>
+
+              &nbsp;{message}
+          </p>
+
+    </div> */}
+
+      {/* Confirm email */}
+      <div className='text-align: center;'>
+        <form action='.' method='POST' role='form'>
+          <h2>Please confirm your email.</h2>
+          <p>Check your email and get the code.</p>
+          <input
+            type='text'
+            name='code'
+            placeholder='enter the 6 digit code ...'
+          />
+          <input type='text' name='user' value='null' />
+          <input type='submit' value='Confirm' name='confirm' />
+        </form>
+      </div>
+
+      {/* modals */}
+      <div className='modals-wrapper'>
+        <span id='close-modal' title='close'>
+          <i className='fa fa-times' aria-hidden='true'></i>
+        </span>
+        {/* <!--login modal--> */}
+        <div className='modal' id='login-modal'>
+          <form action='.' role='form' method='POST'>
+            <h2>Login</h2>
+            <input type='text' name='username' placeholder='username' />
+            <input type='password' name='password' placeholder='password' />
+            <input type='submit' value='login' name='login-form' />
+          </form>
+        </div>
+
+        {/* <!--signup modal--> */}
+        <div className='modal' id='signup-modal'>
+          <form action='.' role='form' method='POST'>
+            <h2>SignUp</h2>
+            <input type='text' name='username' placeholder='username' />
+            <input type='email' name='email' placeholder='email' />
+
+            <input type='password' name='password' placeholder='password' />
+            <input
+              type='password'
+              name='password2'
+              placeholder='confirm password'
             />
-          </a>
+            <input type='submit' value='register' name='signup-form' />
+          </form>
+        </div>
+
+        {/* <!--add password modal--> */}
+        <div className='modal' id='add-password-modal'>
+          <form action='.' role='form' method='POST'>
+            <h2>Add new password</h2>
+            <input type='text' name='url' placeholder='url of website' />
+            <input type='email' name='email' placeholder='email' />
+            <input type='password' name='password' placeholder='password' />
+            <input type='submit' value='save' name='add-password' />
+          </form>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className='container'>
+        {/* <!--One card--> */}
+        <div className='card'>
+          <div className='actions'>
+            &nbsp;Actions{' '}
+            <i className='fa fa-arrow-down' aria-hidden='true'></i>
+            <a href='javascript:;' className='action-item'>
+              Delete
+            </a>
+            <form action='.' method='POST'>
+              <input type='text' name='password-id' value='{{password.id}}' />
+              <input
+                type='submit'
+                id='delete-btn{{password.id}}'
+                name='delete'
+              />
+            </form>
+          </div>
+          <img width='50' height='50' src='{{password.logo}}' alt='' />
+          <div className='name'> </div>
+          {/* <!-- password modal--> */}
+          <input type='email' name='email' value='{{password.email}}' />
+          <i className='fas fa-copy copy' title='copy to clipboard'></i>
+          <input
+            type='password'
+            name='username'
+            value='{{password.password}}'
+          />
+          <i className='fas fa-copy copy' title='copy to clipboard'></i>
+        </div>
       </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <footer>
+        <a href=''>Link To The Project</a>
+      </footer>
     </main>
-  )
+  );
 }
